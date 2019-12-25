0a5e4a90e849043ab5f7c6acc73c17281c9175d9
Benjamin Tang <benjamin.tang.hz@gmail.com>
2019-12-25 16:33:50 +0800

    exported cloud function

--- functions/src/generateStreamToken.ts
+++ functions/src/generateStreamToken.ts
@@ -4,13 +4,15 @@ var stream = require("getstream")
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-  const user_id = request.body.user_id
-  let client = stream.connect(
-    "zvqc7wggt7k9",
-    "24q9r5hu9gxakfsae9ubnue7tde5z7x5fffz6z6hjb5g28v5tzdt3m88xy2ycmaf"
-  )
-  let userToken = client.createUserToken(user_id)
+export const generateStreamToken = functions.https.onRequest(
+  (request, response) => {
+    const user_id = request.body.user_id
+    let client = stream.connect(
+      "zvqc7wggt7k9",
+      "24q9r5hu9gxakfsae9ubnue7tde5z7x5fffz6z6hjb5g28v5tzdt3m88xy2ycmaf"
+    )
+    let userToken = client.createUserToken(user_id)
 
-  response.send(userToken)
-})
+    response.send(userToken)
+  }
+)
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,1 +1,2 @@
 export { processSignUp } from "./getUserData"
+export { generateStreamToken } from "./generateStreamToken"
